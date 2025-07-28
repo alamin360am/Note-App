003f31dd31099501b26105c0349b29cc6982ceec
alamin360am <alaminhimu0@gmail.com>
2025-07-28 16:53:18 +0600

    backend init

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './db/connectDB.js';
+
+const app = express();
+
+dotenv.config();
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect DB:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
